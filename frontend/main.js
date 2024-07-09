4fecf4437c9b0c6ab665b4cf4dce203a5675a165
khadijahW <khadijah028@yahoo.com>
2024-07-08 21:05:17 -0700

    updated

--- frontend/main.js
+++ frontend/main.js
@@ -1,9 +1,9 @@
 // script.js
 
-window.addEventListener("DOMContentLoaded", (event) => {
+window.addEventListener('DOMContentLoaded', (event) => {
   getVisitCount();
 })
-const functionApiUrl = 'https://getresumecounter28.azurewebsites.net/api/GetResumeCounter?code=Te3lxoH2zt68yu6vcFFfDG7GoaKfQAjijSorcgg6eNaOAzFupFAAiQ%3D%3D';
+const functionApiUrl = 'https://getresumecounter28.azurewebsites.net/api/GetResumeCounter?code=nOrob5zZ-b3DuTWHqYwsmkyTvPwNakuG4IQ5FOZdEwkkAzFupXLOOQ%3D%3D';
 const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
@@ -15,8 +15,7 @@ const getVisitCount = () => {
         count = response.count;
         document.getElementById("counter").innerText = count;
     }).catch(function(error){
-    
+        console.log(error);
     });
     return count;
 }
-
