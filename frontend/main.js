d6c3128bff5f80520c93aad556202d763e8569bc
khadijahW <khadijah028@yahoo.com>
2024-07-08 19:53:18 -0700

    updates

--- frontend/main.js
+++ frontend/main.js
@@ -1,21 +1,19 @@
 // script.js
 
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener("DOMContentLoaded", (event) => {
   getVisitCount();
 })
 const functionApiUrl = 'https://getresumecounter28.azurewebsites.net';
 const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
-    let count = 0;
-    fetch(functionApiUrl).then(response => {
-        return response.json()
-    }).then(response =>{
-        console.log("website called function API.");
-        count = response.count;
-        document.getElementById("counter").innerText = count;
-    }).catch(function(error){
-        console.log(error)
-    });
-    return count;
+    fetch(functionApiURL)
+        .then(response => response.json())
+        .then(response => {
+            console.log("Website called function API.");
+            document.getElementById("counter").innerText = response.counter;
+        })
+        .catch(function(error){
+            console.log(error);
+        });
 }
