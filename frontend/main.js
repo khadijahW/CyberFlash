fb1a8f22b06a91e0bbc986d4454bbf41b66665ab
khadijahW <khadijah028@yahoo.com>
2024-07-08 19:53:48 -0700

    updates

--- frontend/main.js
+++ frontend/main.js
@@ -14,6 +14,6 @@ const getVisitCount = () => {
             document.getElementById("counter").innerText = response.counter;
         })
         .catch(function(error){
-            console.log(error);
+    
         });
 }
