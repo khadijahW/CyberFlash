2042e6f20a1da3708ed4729b398ddea229853a30
khadijahW <khadijah028@yahoo.com>
2024-07-08 20:30:01 -0700

    updated

--- frontend/main.js
+++ frontend/main.js
@@ -3,7 +3,7 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   getVisitCount();
 })
-const functionApiUrl = 'https://getresumecounter28.azurewebsites.net';
+const functionApiUrl = 'https://getresumecounter28.azurewebsites.net/api/GetResumeCounter?code=Te3lxoH2zt68yu6vcFFfDG7GoaKfQAjijSorcgg6eNaOAzFupFAAiQ%3D%3D';
 const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
