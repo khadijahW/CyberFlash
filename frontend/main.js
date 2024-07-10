dc887f34df9f908b5e0edc696a7961fed018acb5
khadijahW <khadijah028@yahoo.com>
2024-07-10 13:54:01 -0700

    cors master

--- frontend/main.js
+++ frontend/main.js
@@ -3,7 +3,7 @@
 window.addEventListener('DOMContentLoaded', (event) => {
   getVisitCount();
 })
-const functionApiUrl = 'https://getresumecounter28.azurewebsites.net/api/GetResumeCounter?code=nOrob5zZ-b3DuTWHqYwsmkyTvPwNakuG4IQ5FOZdEwkkAzFupXLOOQ%3D%3D';
+const functionApiUrl = 'https://getresumecounter28.azurewebsites.net/api/GetResumeCounter?code=r3QZz42cQiJYfDAQyitk0CC-ScesrfyOjZQ_Cs9yufdYAzFumHnXGQ%3D%3D';
 const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
